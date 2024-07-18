822a3c231cea7334ee51b78c565cc24ef43e41b4
Meltem <meltemak8@gmail.com>
2024-07-18 17:09:46 +0200

    Initial Commit

---
+++ SwiftUIRecipe/SwiftUIRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRecipeApp.swift
+//  SwiftUIRecipe
+//
+//  Created by Meltem Uğurlu on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
